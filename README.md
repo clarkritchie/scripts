f870e877774344c240f6642140ad4d541c296149
Clark Ritchie <clarkritchie@gmail.com>
2018-10-05 12:42:38 -0700

    copy env vars for EB

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Miscellaneous scripts for this, that.
 
 - `git-br-del.sh` -- used to cleanup old, stale git branches
+- `copyenv.sh` -- copy env vars from one ElasticBeanstalk environment to another
---
+++ copyenv.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+#
+# crazy quick and dirty script to copy Elastic Beanstalk env vars from
+# one environment to another
+#
+# ./copyenv.sh source target
+#
+# outputs the command for you to execute -- just copy and paste
+#
+
+if [ -z "$1" ]; then
+    echo "No from environment specified"
+    echo "Usage $0 from_environment to_environment"
+    exit
+fi
+
+if [ -z "$2" ]; then
+    echo "No to environment specified"
+    echo "Usage $0 from_environment to_environment"
+    exit
+fi
+
+FROM_ENVIRONMENT=$1
+TO_ENVIRONMENT=$2
+
+# make a temp file
+FILE=`mktemp`
+
+# skip first line
+# sqash whitespace around = character
+eb printenv $FROM_ENVIRONMENT | sed -n '1!p' | sed 's/ //g' > $FILE
+echo $FILE
+cat $FILE
+
+# print the command the user can copy-paste into a shell to update the new environment
+
+echo
+echo "Run this command to update $TO_ENVIRONMENT with env vars from $FROM_ENVIRONMENT:"
+echo
+echo -n "eb setenv "
+
+# append \ to each line
+awk '{print $0, "\\"}' $FILE
+echo -n "-e $TO_ENVIRONMENT"
+echo
+
+# cleanup temp file
+rm -f $FILE
