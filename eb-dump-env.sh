80d9c58cedbaacfd22c1db1e7098fdc4a1ab5e54
Clark Ritchie <clarkritchie@users.noreply.github.com>
2018-10-27 17:42:45 -0700

    Update eb-dump-env.sh

--- eb-dump-env.sh
+++ eb-dump-env.sh
@@ -3,12 +3,13 @@
 # Dump the env vars for each ElasticBeanstalk environment
 # to a file named [environment].env
 #
+
 eb list | sed 's/* //' | while read -r ebenv ; do
 	TMP=`mktemp`
 	FILE=$ebenv.env
 	echo "Dumping env vars for $ebenv"
-    eb printenv $ebenv \
-   	| sort -k1,1 -k2n > $TMP # sort them alphabetically
-   	sed '$d' $TMP > $FILE # remove the last line which contains "Environment"
+    	eb printenv $ebenv \
+   		| sort -k1,1 -k2n > $TMP # sort alphabetically
+   		sed '$d' $TMP > $FILE # remove the last line, which contains "Environment"
    	rm -f $TMP
 done
